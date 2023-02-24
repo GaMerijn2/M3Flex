cc01aa8bdf16c3c3784024c504b973a0c7a17048
Merijn Florie <34626@ma-web.nl>
2023-02-24 10:30:50 +0100

    Nicely added

---
+++ CSharpOpdrachten/CSharpOpdrachten/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharpOpdrachten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
