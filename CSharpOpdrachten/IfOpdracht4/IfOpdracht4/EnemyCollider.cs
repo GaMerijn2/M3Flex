4a744ffd3b81cd088ebd870a8d4c3317121c058b
Merijn Florie <34626@ma-web.nl>
2023-03-17 10:07:53 +0100

    alles toegevoegd

---
+++ CSharpOpdrachten/IfOpdracht4/IfOpdracht4/EnemyCollider.cs
@@ -0,0 +1,34 @@
+﻿namespace IfOpdracht4
+{
+    internal class EnemyCollider
+    {
+        internal bool CanPassWall(Enemy enemy)
+        {
+            if (enemy.flying || enemy.incoporial || enemy.subterranian)
+            {
+                return true;
+            }
+            else
+            {
+                return false;//vervang false, een enemy mag door een muur als 1 van  (flying,incoporial,subterranian) true is;
+
+            }
+            //gebruik hier een || (OR)
+            //je kan enemy.??? gebruiken om de eigenschap te testen, bv voor flying wordt het enemy.flying
+        }
+        internal bool CanPassWater(Enemy enemy)
+        {
+
+            if (enemy.flying || enemy.incoporial || enemy.subterranian || enemy.swimmer)
+            {
+                return true;
+            }
+            else
+            {
+                return false;//vervang false, een enemy mag door een muur als 1 van  (flying,incoporial,subterranian,swimmer) true is;
+            }
+            //gebruik hier een || (OR)
+            //je kan enemy.??? gebruiken om de eigenschap te testen, bv voor flying wordt het enemy.flying
+        }
+    }
+}
